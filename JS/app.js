e07ae6694ea2a65e7a7fb1a05afcfcb9412a044d
DavyDeMattos <110909256+DavyDeMattos@users.noreply.github.com>
2023-02-20 11:36:18 +0100

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+const app = {
+    init: function () {
+			app.handleSliderImage();
+
+    },
+
+};
+    // on initialise l'app dès que le document est prêt
+    document.addEventListener('DOMContentLoaded', app.init);
