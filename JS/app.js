b8654f344982d2e6be43bb43f3614ea38e44ca72
DavyDeMattos <davy.demattos@gmail.com>
2023-02-24 17:01:03 +0100

    WIP setting data in JS

--- JS/app.js
+++ JS/app.js
@@ -23,12 +23,113 @@ const app = {
         // console.log(clickedArticle);
         // On sélectionne le container
         const window = document.querySelector('#windowInformation');
-        // const divElement = document.createElement('div');
-        // divElement.classList.add('characterInformation');
-
+        window.innerHTML = ''
         // On place la div dans le container.
         // containerCharacter.appendChild(divElement);
-        // app.showInformation(clickedArticle);
+        app.showInformation(clickedArticle);
+    },
+
+    showInformation : function (name){
+        // Nous recevons en paramètre le nom de la personne cliqué.
+        console.log('Le charactère cliqué est : ' + name);
+
+        // Sélection de la div de character
+        const container = document.querySelector('.characterInformation');
+
+        container.innerHTML = "";
+        // Création des élements de la div
+        const characterName = document.createElement('h2');
+        const anniversaire = document.createElement('p');
+        const favoriteTitle = document.createElement('h3');
+        const ulElement = document.createElement('ul');
+        ulElement.classList.add('favorite--gift');
+
+        let projects = [
+            // Projet fini
+            {
+                slug : "choose_your_romance",
+                title : "Choose your romance",
+                resume : "Dans le jeu Stardey Valley, il est possible d'augmenter son affinité avec des personnages en leur parlans ou leur offrant des cadeaux qu'ils apprécient. Après avoir en avoir eu assez d'ouvrir moult onglets des page wiki pour avoir accès à ces informations j'ai développé cet outil sans prétention. L'utilisateur peut choisir le personnage souhaité en défilant via les flèches et cliquer sur le personnage voulu.",
+                status : "Fini",
+                link: "Stardew_Valley/html/index.html",
+                tools : [
+                    "HTML",
+                    "CSS",
+                    "Bootstrap",
+                    "JS"
+                ]
+            },
+            {
+                slug : "visiotec",
+                title : "Visiotec",
+                resume : "Dans le cadre de ma formation, nous avons développé ce site avec une équipe de 5 personnes. Il a pour but de faire office de marque-page virtuelle. Chaque utilisateur connecté peut répertorier des séries et films et indiquer à quel saison et épisode il s'est arrêté.",
+                status : "En cours de publication",
+                links : "",
+                tools : [
+                    "SASS",
+                    "React",
+                    "React-Redux",
+                    "React Router Dom",
+                    "Axios",
+                    "Symphony",
+                    "Symfony (framework)",
+                    "MySQL ( Base de données)",
+                    "JWT Token (authentication / sécurité)",
+                    "Thunder client (Pour tester les requêtes)"
+                ]
+            },
+            // Projet en cours de développement
+            {
+                slug : "hotel",
+                title : "Hotel",
+                resume : "Site servant à m'entrainer ayant pour thème l'hotellerie",
+                status : "En cours de développement",
+                links : "",
+                tools : [
+                ]
+            },
+            {
+                slug : "ca_trompe_enormement",
+                title : "Ca trompe énormément",
+                resume : "Site destiné au groupe de musique éponyme qui servira à communiquer leur actualité tels que leurs images, dates de prestation ou contact",
+                status : "En cours de développement",
+                links : "",
+                tools : [
+                ]
+            }
+        ]
+
+        for (const character of tableau) {
+            // Si le nom du tableau corresponds au nom cliqué
+            if (name == character.name) {
+                // Ajout du titre
+                container.appendChild(characterName);
+                characterName.textContent = character.name
+
+                // Ajout de l'anniversaire
+                container.appendChild(anniversaire);
+                anniversaire.textContent = `L'anniversaire de ${character.name} est le ${character.anniversaire}`
+                
+                container.appendChild(favoriteTitle);
+                favoriteTitle.textContent = 'Voici la liste de ses cadeaux favoris'  ;       
+
+                // Ajout de la liste des cadeau favoris
+                container.appendChild(ulElement);   
+
+
+                for (const item of character.likes) {
+                    let liElement = document.createElement('li');
+                    ulElement.appendChild(liElement);
+                    liElement.innerHTML = `<img src='../images/${item}.png' /> <h4>${item}</h4>`
+                }
+
+                console.log(character.anniversaire);
+                console.log(character.likes);
+            }
+            // console.log(character.name);
+        }
+        // console.log(tableau[1].likes);
+
     },
 
     handleSliderImage : function(){
